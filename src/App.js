71f56361286b477ac08f337fd187eacb3df775c0
ljh00112233 <151446861+ljh00112233@users.noreply.github.com>
2023-11-29 17:23:59 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// import './sass/materialize.scss'
+// import "./App.scss";
+// import Button from "./04/Button";
+// import Input from "./04/InputWithStyle"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <nav>
+//         <div className="nav-wrapper">
+//           <div>
+//             리액트 시작
+//           </div>
+//         </div>
+//       </nav>
+//       <Input label="나이" name="age" errorMessage="나이를 입력하세요"/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import styled from 'styled-components';
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: black;
+  border-radius: 50%;
+`;
+function App(){
+  return <Circle />;
 }
-
 export default App;
