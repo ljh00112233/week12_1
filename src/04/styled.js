f271962a535482dbbbd1e3c329cfe4184ef686ef
ljh00112233 <151446861+ljh00112233@users.noreply.github.com>
2023-11-29 17:23:44 +0900

    Add files via upload

--- src/04/styled.js
+++ src/04/styled.js
@@ -1,10 +1,25 @@
-const red = '빨간색';
-const blue = '파란색';
-function emphasize(texts, ...values){
+// const red = '빨간색';
+// const blue = '파란색';
+// function emphasize(texts, ...values){
+//     let result = '';
+//     for(let i=0; i<texts.length; i++){
+//         result += texts[i] + (values[i]?`<b>${values[i]}</b>`:'');
+//     }
+//     console.log(result);
+// }
+// emphasize`제가 좋아하는 색은 ${red}과 ${blue}입니다.`
+
+function StyledDiv(styles, ...fns){
+    const color = 'red';
     let result = '';
-    for(let i=0; i<texts.length; i++){
-        result += texts[i] + (values[i]?`<b>${values[i]}</b>`:'');
+    for(let i=0; i<styles.length; i++){
+        result += styles[i] + (fns[i]?fns[i](color):'');
     }
     console.log(result);
 }
-emphasize`제가 좋아하는 색은 ${red}과 ${blue}입니다.`
+StyledDiv`
+    width: 200px;
+    height: 10px;
+    background: ${(value)=>value};
+    border-radius: 50%;
+    `
