b71c24138c9f7eb0442365c64d221b98ff2312e4
ljh00112233 <151446861+ljh00112233@users.noreply.github.com>
2023-11-29 17:27:16 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+// import React from "react";
+// import './sass/materialize.scss'
+// import "./App.scss";
+// import Button from "./04/Button";
+// import Input from "./04/InputWithStyle"
+
+// function App() {
+//   return (
+//     <div>
+//       <nav>
+//         <div className="nav-wrapper">
+//           <div>
+//             리액트 시작
+//           </div>
+//         </div>
+//       </nav>
+//       <Input label="나이" name="age" errorMessage="나이를 입력하세요"/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import styled from 'styled-components';
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${(props)=>props.color || 'black'};
+  border-radius: 50%;
+`;
+function App(){
+  return <Circle color="red"/>;
+}
+export default App;
